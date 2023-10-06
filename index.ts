60502c738fd150dd8d4ec9316cd8ef3634e218d9
Ruben Talboom <ruben.talboom@student.ap.be>
2023-10-06 11:57:02 +0000

    card now uses full urls and index is now named index

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("port", 3000);
 
 app.get("/", (req, res) => {
-    res.render("homeProject",{title: "Home"});
+    res.render("index",{title: "Home"});
 });
 
 app.get("/home",(req,res)=>{
