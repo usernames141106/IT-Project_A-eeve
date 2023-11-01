034a15e45132af82bad4730a4860317a42f8d50f
Kenneth Moorkens <kenneth.moorkens@student.ap.be>
2023-11-01 14:33:25 +0000

    pokemon battle js basis

--- public/js/hpbar.js
+++ public/js/hpbar.js
@@ -1,7 +1,29 @@
-// Function to update the HP bar and change the color depending on the value (currently not working)
-function updateHPBar(percentage) {
-  const hpBar = document.getElementById("hp-bar");
-  let label = document.getElementById("HPLabel");
+// initialize elements to animate
+// const divPokemon = document.getElementById("imageOwnPokemon");
+
+// initialize HP values
+let ownPokemonHP = 100;
+let enemyPokemonHP = 100;
+let isOwnPokemonTurn = true;
+
+// set initial value for own HP bar
+const hpBar = document.getElementById("hp-bar");
+let label = document.getElementById("HPLabel");
+hpBar.style.width = ownPokemonHP + "%";
+label.textContent = ownPokemonHP;
+hpBar.style.backgroundColor = "lightgreen";
+
+// set initial value for the enemy HP bar
+const hpBarEnemy = document.getElementById("hp-bar-enemy");
+let labelEnemy = document.getElementById("HPLabelEnemy");
+hpBarEnemy.style.width = enemyPokemonHP + "%";
+labelEnemy.textContent = enemyPokemonHP;
+hpBarEnemy.style.backgroundColor = "lightgreen";
+
+// function to update the HP bar and change the color depending on the value
+function updateHPBar(percentage, barId) {
+  const hpBar = document.getElementById(barId);
+  let label = document.getElementById(`HPLabel${barId === "hp-bar" ? "" : "Enemy"}`);
   hpBar.style.width = percentage + "%";
   label.textContent = percentage;
 
@@ -14,22 +36,59 @@ function updateHPBar(percentage) {
   }
 }
 
-function updateHPBarEnemy(percentage) {
-  const hpBar = document.getElementById("hp-bar-enemy");
-  let labelEnemy = document.getElementById("HPLabelEnemy");
-  hpBar.style.width = percentage + "%";
-  labelEnemy.textContent = percentage;
+// function to simulate an attack from your Pokémon
+function attackOwnPokemon() {
+  const damage = 30;
+  enemyPokemonHP -= damage;
+  updateHPBar(enemyPokemonHP, "hp-bar-enemy");
 
-  if (percentage <= 10) {
-    hpBar.style.backgroundColor = "RGB(255,105,105)";
-  } else if (percentage <= 50) {
-    hpBar.style.backgroundColor = "orange";
-  } else {
-    hpBar.style.backgroundColor = "lightgreen";
+  if (enemyPokemonHP <= 0) {
+    setTimeout(() => {
+      alert("You defeated the enemy Pokémon! You win!");
+    }, 2000);
+    enemyPokemonHP == 0;
+    return;
   }
 }
 
-const hpValue = 100; // Alter the HP value
-updateHPBar(hpValue);
-updateHPBarEnemy(hpValue);
+// function to simulate an attack from the enemy Pokémon
+function attackEnemyPokemon() {
+  const damage = 30;
+  ownPokemonHP -= damage;
+  updateHPBar(ownPokemonHP, "hp-bar");
 
+  if (ownPokemonHP <= 0) {
+    setTimeout(() => {
+      alert("Your Pokémon fainted. Game over!");
+    }, 2000);
+    ownPokemonHP == 0;
+    return;
+  }
+}
+
+// function to perform a sequence of attacks from both Pokémon
+function battle() {
+  for (let i = 0; i < 100; i++) {
+    setTimeout(() => {
+      if (isOwnPokemonTurn && enemyPokemonHP > 0) {
+        // divPokemon.style.marginLeft = "100px";  // animate an attack
+        attackOwnPokemon();
+      } else if (!isOwnPokemonTurn && ownPokemonHP > 0) {
+        attackEnemyPokemon();
+      } else {
+        return;
+      }
+      isOwnPokemonTurn = !isOwnPokemonTurn;
+    }, i * 1500); // wait for 1,5 seconds between each attack
+  }
+}
+
+// start the fight
+const fightButton = document.getElementById("fightButton");
+fightButton.addEventListener("click", () => {
+  hpBar.style.transition = "1s ease-out";  // fasten the animation
+  hpBarEnemy.style.transition = "1s ease-out";
+  if (ownPokemonHP > 0 && enemyPokemonHP > 0) {
+    battle();
+  }
+});
