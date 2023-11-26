e4711d40aec38b2aaf7dd29d3cc66e0fc00a0242
Kenneth Moorkens <kenneth.moorkens@student.ap.be>
2023-11-26 16:02:36 +0000

    commentaar uit index.ts verwijderd

--- index.ts
+++ index.ts
@@ -1,85 +1,10 @@
 import express from 'express';
 import session from 'express-session';
-//import bcrypt from 'bcrypt';
 import { MongoClient, ObjectId } from 'mongodb';
 import { IPokemon, IUser } from './interface';
 import { GetPokemonFromApi, PokemonList } from './db';
 
 const app = express();
-
-/* poging setup cookies
-// MongoDB connection setup
-const mongoUrl = 'mongodb+srv://itProject:f5pajH6wH8eHzpI5@cluster0.jnpguhk.mongodb.net/?retryWrites=true&w=majority';
-const dbName = 'itProject';
-
-const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect().then(() => {
-    console.log('Connected to MongoDB');
-});
-
-// Express app setup
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Session setup
-app.use(session({
-    secret: 'pajH6wH8eHzp',
-    resave: false,
-    saveUninitialized: true,
-}));
-
-// Route for registration
-app.post('/register', async (req, res) => {
-    const { name, password } = req.body;
-
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-    
-    const user: IUser = {
-        name,
-        passwordHash,
-        pokemons: [],
-    };
-    
-    const usersCollection = client.db(dbName).collection<IUser>('users');
-
-    try {
-        const result = await usersCollection.insertOne(user);
-        console.log('User successfully registered:', result.insertedId);
-        res.redirect('/login');
-    } catch (error) {
-        console.error('Registration failed:', error);
-        res.redirect('/register'); // Handle registration failure
-    }
-});
-
-// Route for login
-app.post('/login', async (req, res) => {
-    const { name, password } = req.body;
-
-    const usersCollection = client.db(dbName).collection<IUser>('users');
-    const user = await usersCollection.findOne({ name });
-
-    if (user && await bcrypt.compare(password, user.passwordHash)) {
-        req.session.userId = user._id;
-        console.log('Successfully logged in');
-        res.redirect('/dashboard');
-    } else {
-        console.log('Login failed');
-        res.redirect('/login');
-    }
-});
-
-// Route for dashboard
-app.get('/dashboard', (req, res) => {
-    if (req.session.userId) {
-        res.send('Welcome to your dashboard');
-    } else {
-        res.redirect('/login');
-    }
-});
-*/
 
 app.use(express.static("public"));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
