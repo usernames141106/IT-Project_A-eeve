2c3bda186027f5196b6e506bb8efc131e37d9ec2
Kenneth Moorkens <kenneth.moorkens@student.ap.be>
2023-12-06 09:18:09 +0000

    script loadPokemon

--- public/js/loadPokemon.js
+++ public/js/loadPokemon.js
@@ -1,11 +1,14 @@
-document.getElementById('buttonSearch').addEventListener('click', function() {
-  var selectedPokemon = document.getElementById('zoekPokemon').innerHTML;
-  console.log(selectedPokemon);
- 
-  var pokemon = PokemonList.find(p => p.name === selectedPokemon);
- 
-  if(pokemon) {
-    document.getElementById('pokemonImage').src = pokemon.image;
-  }
- });
- 
+fetch("/pokemonList")
+  .then(response => response.json())
+  .then(PokemonList => {
+    document.getElementById('buttonSearch').addEventListener('click', function () {
+      var selectedPokemon = document.getElementById('zoekPokemon').value;
+      console.log(selectedPokemon);
+
+      var pokemon = PokemonList.find(p => p.name === selectedPokemon);
+
+      if (pokemon) {
+        document.getElementById('pokemonImage').src = pokemon.image;
+      }
+    });
+  });
