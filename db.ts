4765d78ad0b79aa3402aaa6207998f2949da6c41
Talboom Berrie <berrie.talboom@student.ap.be>
2023-12-09 14:11:52 +0000

    removed inconsistancy between stored data in session and mongodb

--- db.ts
+++ db.ts
@@ -60,6 +60,9 @@ export async function LoadUserFromMongoDB(email: string, password: string): Prom
             const hash: string = crypto.pbkdf2Sync(password,
             outputUser.salt, 1000, 64, `sha512`).toString(`hex`);
             if(outputUser.hash == hash) {
+                if(outputUser.currentPokemon === null) {
+                    outputUser.currentPokemon = undefined;
+                }
                 return outputUser;
             }
         }
